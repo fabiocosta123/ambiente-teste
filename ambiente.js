98b02f4c3813d75098d6c8ce82f47fbaa22b8e08
Fábio Costa <fabiocostasilvaoficial@gmail.com>
2022-03-28 09:53:32 -0300

    funçao de calcular media

--- ambiente.js
+++ ambiente.js
@@ -1,5 +1,24 @@
-localStorage.setItem('Produto', 'valor')
+/*localStorage.setItem('Produto', 'valor')
 function calcular(){
-    const quantidade = document.querySelector('#quantidade')
+    const quantidade = document.getElementById('quantidade')
+    const valor= document.getElementById('valoUnitario')
     alert(quantidade);
+}*/
+function calcular(){
+  let media = calculaValor() / calculakm()
+  alert(media)
+  return media
+   
+}
+function calculaValor(){
+    let qtd = document.getElementById('quantidade')
+    let valor = document.getElementById('valorUnitario')
+    let total = Number(qtd.value) * Number(valor.value)
+    return total
+}
+function calculakm(){
+    let kmi = document.getElementById('kmi')
+    let kmf = document.getElementById('kmf')
+    let kmr = Number(kmf.value) - Number(kmi.value)
+    return kmr
 }
