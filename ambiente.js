04f7f983f011d681d338bc4ef886d2de069951dc
Fábio Costa <fabiocostasilvaoficial@gmail.com>
2022-03-29 18:02:56 -0300

    criando funçao calcula media

--- ambiente.js
+++ ambiente.js
@@ -1,34 +1,18 @@
 
+
 let qtd = document.getElementById('quantidade')
-let valor = document.getElementById('valorUnitario')
 let kmi = document.getElementById('kmi')
 let kmf = document.getElementById('kmf')
-// está calculando o valor do km rodado
-function calculaPrecoKm() {
-    let media = calculakm() / calculaValor()
-    return media
-}
-function calculaValor() {
-
-    let total = Number(qtd.value) * Number(valor.value)
-    return total
-}
-function calculakm() {
-
-    let kmr = Number(kmf.value) - Number(kmi.value)
-    return kmr
-}
-
-// calcular a media 
-
-function calculaMediaKm() {
-    let mediakm = kmr / Number(qtd.value)
-    return mediakm
-}
+let preco = document.getElementById('valorUnitario')
+let res = document.getElementById('res')
 
 function calcular(){
-    calculaPrecoKm()
-    calculaValor()
-    calculaMediaKm()
-    alert(`O valor gasto por litro é de R$ ${media.toFixed(2)} e consumo médio de ${mediakm.toFixed(2)}`)
+    calculaMedia()
 }
+function calculaMedia() {
+    let mediaConsumo = (Number(kmf.value) - Number(kmi.value)) / Number(qtd.value)
+    res.innerHTML = `A media de consumo é de ${mediaConsumo} km/l`
+    return mediaConsumo
+}
+
+
